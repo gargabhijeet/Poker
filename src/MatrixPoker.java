4110b2028b0be9e4ca119ecf57ff16a431954470
gargabhijeet <gargabhijeet@users.noreply.github.com>
2018-12-12 17:11:55 +0530

    GamePlay

--- src/MatrixPoker.java
+++ src/MatrixPoker.java
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import javax.security.auth.login.LoginException;
+import javax.swing.JOptionPane;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -35,12 +36,12 @@ import com.google.common.io.Files;
 
 public class MatrixPoker {
 	
-	//Hello 
 	static Random ran = new Random();
 	static int ranint = ran.nextInt(1000);
 	static String username = "Auto" + ranint;
 	static int bankacc = randomNumberInRange(1000000000, 2100000000);
 	static String account = Integer.toString(bankacc);
+	static int  playeronseat = 0;
 
 	public static int randomNumberInRange(int min, int max) {
 		Random random = new Random();
@@ -83,15 +84,25 @@ public class MatrixPoker {
 		System.out.println("Terms Accepting Done");
 	}
 
-	public static void login(WebDriver driver, WebDriverWait wait, String user) {
+	public static void login(WebDriver driver, WebDriverWait wait) {
 
+		String user = JOptionPane.showInputDialog("Enter Username");
+		System.out.println("USERNAME : "+user);
+		String password = JOptionPane.showInputDialog("Enter Password");
+		System.out.println("PASSWORD : "+password);
+		String captcha = JOptionPane.showInputDialog("Enter Captcha");
+		System.out.println("CAPTCHA : "+captcha);
+		
+		
 		driver.findElement(By.id("uname")).sendKeys(user);
-		driver.findElement(By.id("upassword")).sendKeys("123123");
+		driver.findElement(By.id("upassword")).sendKeys(password);
+		driver.findElement(By.id("logincaptcha1")).sendKeys(captcha);
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("logsubmit")));
 		driver.findElement(By.id("logsubmit")).click();
 		System.out.println("Login Done For Player : " + user);
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("agree")));
 		driver.findElement(By.id("agree")).click();
-		System.out.println("Terms Accepting Done");
+		System.out.println("ACCEPTING TERMS DONE");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
@@ -320,9 +331,7 @@ public class MatrixPoker {
 			}
 		}
 	}
-	
-	//This test case will cover the complete functionality for Memo module.
-	
+		
 	public static void Memo(WebDriver driver, WebDriverWait wait) throws InterruptedException {
 		Thread.sleep(3000);
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("menu_MEMO")));
@@ -386,26 +395,97 @@ public class MatrixPoker {
 	}
 	
 	public static void OpenTable(WebDriver driver, WebDriverWait wait) throws InterruptedException {
-		//Thread.sleep(3000);
+		Thread.sleep(3000);
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("menu_LOBBY")));
 		driver.findElement(By.id("menu_LOBBY")).click();
-		System.out.println("Navigation to Lobby Done");
-		Thread.sleep(3000);
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'utill_playgame_btn_0_')]")));
-		driver.findElement(By.xpath("//*[contains(@id,'utill_playgame_btn_0_')]")).click();
-		//Thread.sleep(3000);
-		//String tablename = driver.findElement(By.id("utill_table_body_td_0_")).getText();
-		//System.out.println(tablename + "  Table Opening Done");
+		System.out.println("NAVIGATION TO LOBBY DONE");
 		Thread.sleep(3000);
 		
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("aq_chat")));
+		List<WebElement> rows = driver.findElements(By.xpath("//*[contains(@id,'utill_table_body_tr_')]"));	
+		int count = rows.size();
+		System.out.println("TOTAl NUMBER OF TABLE : "+count);
 		
-		JavascriptExecutor js = (JavascriptExecutor) driver;
+		for(int i=0;i<count;i++) {
+			String tablename = driver.findElement(By.id("utill_table_body_td_"+i+"_0")).getText();
+			System.out.println("TABLENAME : "+i+" : "+tablename);
+		}
+
+//		String numberOfPlayer = driver.findElement(By.id("utill_table_body_td_0_2")).getText();
+//		System.out.println(numberOfPlayer+" Player Seated");
+//		String tablename = driver.findElement(By.id("utill_table_body_td_0_0")).getText();
+//		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'utill_table_body_td_0_4')]")));
+//		driver.findElement(By.xpath("//*[contains(@id,'utill_table_body_td_0_4')]")).click();
 		
-		Object val = js.executeScript("return myRoot[\"seat_1\"])");
-		System.out.println(val);
+		
+		// [take Seat on First Table]
+		String tablename = driver.findElement(By.id("utill_table_body_tr_0")).getText();
+		driver.findElement(By.id("utill_table_body_tr_0")).click();
+		System.out.println("OPENED TABLENAME : "+tablename.toString());
+		
+		
+		// [Search table and take sit]
+//		Boolean tableFound = false;
+//		int num =0;
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//
+//		while (tableFound == false) {
+//			String tablename = driver.findElement(By.id("utill_table_body_td_"+num+"_0")).getText();
+//			System.out.println(tablename);
+//			if(tablename.equals("LOAS15")) {
+//				driver.findElement(By.id("utill_table_body_tr_"+num+"")).click();
+//				tableFound = true;
+//			}
+//			num = num+1;
+//			js.executeScript("var y=document.getElementsByClassName(\"slimScrollDiv\");y.scrollTop=document.getElementById(\"utill_table_body_tr_"+num+"\").offsetTop;");
+//			
+//		}
 	}
 
+	public static void TakeSeat(WebDriver driver, WebDriverWait wait) throws InterruptedException {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;  
+		Thread.sleep(10000);
+
+		//FINDING SEAT AVAILABLITY TO SEAT
+		for(int i=1;i<=9;i++) {
+			Boolean seatAvailability = (Boolean) js.executeScript("return myRoot.seat_"+i+".visible");
+
+			if(seatAvailability == true) {
+				System.out.println("Player Seating at "+i+" Position on table");
+				js.executeScript("myRoot.seat_"+i+".dispatchEvent('click');", "return myRoot.seat_"+i);
+				playeronseat = i;
+				break;
+			}
+		}
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'utill_pok_')]")));		
+		driver.findElement(By.xpath("//*[contains(@id,'utill_pok_')]")).click();
+		System.out.println("Player Seat Taking Done");		
+	}
+	
+	public static void GamePlay(WebDriver driver, WebDriverWait wait) throws InterruptedException{
+		
+		Thread.sleep(5000);
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String playerBalance =  (String) js.executeScript("return myRoot.player_"+playeronseat+".panel.pBank.text");
+		int balance = Integer.parseInt(playerBalance);
+		System.out.println("Player balance : "+ balance);
+		while (balance > 0) {
+			
+			Boolean action = (Boolean) js.executeScript("return myRoot.paHolderMain.ActionControls.visible");
+			
+			if(action == true) {
+				js.executeScript("myRoot.paHolderMain.ActionControls.btnCheckCall.dispatchEvent('click');");
+				System.out.println("Player Performed Action");
+				
+				playerBalance =  (String) js.executeScript("return myRoot.player_1.panel.pBank.text");
+				balance = Integer.parseInt(playerBalance);
+				System.out.println("Player balance : "+ balance);
+			}
+		}
+	}
+	
 	public static void chat(WebDriver driver, WebDriverWait wait) {
 		wait.until(ExpectedConditions.elementToBeClickable(By.id("openchat")));
 		driver.findElement(By.id("openchat")).click();
@@ -420,52 +500,48 @@ public class MatrixPoker {
 	}
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
-		System.out.println("Hello");
-		
-		String exePath = "C:\\chromedriver.exe";
+		String exePath = "src\\Driver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", exePath);
 
-		/* WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		 WebDriverWait wait = new WebDriverWait(driver, 20);
-		 System.out.println("Browser Opening Done");
+		 System.out.println("BROWSER OPENING DONE");
 
 		 driver.manage().window().maximize();
-		 driver.get("http://stage.freepoker99.com/");
-		 System.out.println("Poker website Opening Done"); */
-
-		File f = new File("D://user.txt");
-		BufferedReader b = new BufferedReader(new FileReader(f));
-		String readLine = ""; 
-
-		while ((readLine = b.readLine()) != null) {
-
-			WebDriver driver = new ChromeDriver();
-			WebDriverWait wait = new WebDriverWait(driver, 20);
-			System.out.println("Browser Opening Done");
-
-			driver.manage().window().maximize();
-			driver.get("http://stage.freepoker99.com/");
-			System.out.println("Poker website Opening Done");
-
-			String user = readLine;
-
-			login(driver, wait, user);
-			//register(driver,wait);
-			//NavigateToCashier(driver, wait);
-			//MakeDepositOnCashierPage(driver, wait);
-			//ApproveDeposit();
-			//NavigateToProfile(driver, wait);
-			//NavigateToJackpot(driver, wait);
-			//NavigateToReferral(driver, wait);
-			//NavigateToMemo(driver, wait);
-			//Memo(driver, wait);
+		 driver.get("https://pokermatrix66.org/");
+		 System.out.println("POKER WEBSITE OPENING DONE");
+		 
+		 
+			login(driver, wait);
 			NavigateToLobby(driver, wait);
 			OpenTable(driver, wait);
-			//chat(driver,wait);
-			//LogOut(driver, wait);
-		}
-		b.close(); 
+			TakeSeat(driver,wait);
+			GamePlay(driver,wait);
+
+//		File f = new File("D://user.txt");
+//		BufferedReader b = new BufferedReader(new FileReader(f));
+//		String readLine = ""; 
+//
+//		while ((readLine = b.readLine()) != null) {
+//
+//			WebDriver driver = new ChromeDriver();
+//			WebDriverWait wait = new WebDriverWait(driver, 20);
+//			System.out.println("Browser Opening Done");
+//
+//			driver.manage().window().maximize();
+//			driver.get("https://pokermatrix66.org/");
+//			System.out.println("Poker website Opening Done");
+//
+//			String user = readLine;
+//
+//			login(driver, wait, user);
+//			NavigateToLobby(driver, wait);
+//			OpenTable(driver, wait);
+//			TakeSeat(driver,wait);
+//			GamePlay(driver,wait);
+//
+//		}
+//		b.close(); 
 
 	}
 }
